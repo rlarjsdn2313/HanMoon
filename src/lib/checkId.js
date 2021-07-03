aadd2eb03bf4ff0ad8f1b8e64962f4a3d39bd1ca
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-07-03 07:28:59 +0000

    slight change

--- src/lib/checkId.js
+++ src/lib/checkId.js
@@ -2,7 +2,6 @@ let fs = require('fs');
 
 let checkId = function(path, id) {
 	filelist = fs.readdirSync(path);
-	console.log(filelist);
 	
 	for (let i=0; i<filelist.length; i++) {
 		if (filelist[i] === 'version.json') {
